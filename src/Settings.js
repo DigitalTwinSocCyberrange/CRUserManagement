411e122cac7fa35b057cb1c36b23f5f82fbbc3f6
lenaglas <lena.glas@web.de>
2022-05-30 08:56:56 +0200

    update quiz url

--- src/Settings.js
+++ src/Settings.js
@@ -1,6 +1,6 @@
 const settings = {
 
-    urlPreQuiz: "https://quizizz.com/join?gc=24177005",
+    urlPreQuiz: "https://quizizz.com/join?gc=57862509",
     scenarioName: "ICEBERG",
     naming: "name",
     namingPlaceholder: "e.g. mglas"
